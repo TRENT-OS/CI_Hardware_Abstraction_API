109ee85132bcefa38beb643575761d5f0e067077
Felix Schladt <felix.schladt@hensoldt.net>
2024-02-27 15:32:40 +0100

    [DOC] Copyright

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
+
+#
+# Copyright (C) 2024, HENSOLDT Cyber GmbH
+# 
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# For commercial licensing, contact: info.cyber@hensoldt.net
+#
+
 import argparse
  
 import uvicorn
--- uart_proxy.py
+++ uart_proxy.py
@@ -1,3 +1,11 @@
+#
+# Copyright (C) 2024, HENSOLDT Cyber GmbH
+# 
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# For commercial licensing, contact: info.cyber@hensoldt.net
+#
+
 from fastapi import FastAPI, HTTPException
 import serial
 import threading
